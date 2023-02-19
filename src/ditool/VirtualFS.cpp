8fbd11d39855e63b4c9b3f4acb135e322460f834
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2023-02-19 07:24:09 +0000

    Working around strange bug in macOS.
    git-svn-id: https://svn.code.sf.net/p/previous/code/branches/branch_softfloat@1298 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/ditool/VirtualFS.cpp
+++ src/ditool/VirtualFS.cpp
@@ -306,7 +306,7 @@ FileAttrs::FileAttrs(const string& serialized) {
 
 string FileAttrs::serialize() const {
     char buffer[128];
-    snprintf(buffer, sizeof(buffer), "0%o:%d:%d:%d", mode, uid, gid, rdev);
+    snprintf(buffer, sizeof(buffer), "0%o:%d:%d:%d:", mode, uid, gid, rdev);
     return string(buffer);
 }
 
