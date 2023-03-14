7c599e42bc744728567f6ec9774510aa04d5ffb7
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2023-03-14 17:32:13 +0000

    Updating to latest WinUAE/Hatari.
    git-svn-id: https://svn.code.sf.net/p/previous/code/branches/branch_softfloat@1312 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -1591,6 +1591,7 @@ void invalidate_cpu_data_caches(void)
 	}
 }
 
+#ifdef DEBUGGER
 static void flush_cpu_cache_debug(uaecptr addr, int size)
 {
 	debugmem_flushcache(0, -1);
@@ -1598,6 +1599,7 @@ static void flush_cpu_cache_debug(uaecptr addr, int size)
 	debug_smc_clear(0, -1);
 #endif
 }
+#endif
 
 void flush_cpu_caches(bool force)
 {
@@ -1608,12 +1610,16 @@ void flush_cpu_caches(bool force)
 			for (int i = 0; i < CACHELINES020; i++)
 				caches020[i].valid = 0;
 			regs.cacr &= ~0x08;
+#ifdef DEBUGGER
 			flush_cpu_cache_debug(0, -1);
+#endif
 		}
 		if (regs.cacr & 0x04) { // clear entry in instr cache
 			caches020[(regs.caar >> 2) & (CACHELINES020 - 1)].valid = 0;
 			regs.cacr &= ~0x04;
+#ifdef DEBUGGER
 			flush_cpu_cache_debug(regs.caar, CACHELINES020);
+#endif
 		}
 	} else if (currprefs.cpu_model == 68030) {
 		if ((regs.cacr & 0x08) || force) { // clear instr cache
@@ -1626,12 +1632,16 @@ void flush_cpu_caches(bool force)
 				}
 			}
 			regs.cacr &= ~0x08;
+#ifdef DEBUGGER
 			flush_cpu_cache_debug(0, -1);
+#endif
 		}
 		if (regs.cacr & 0x04) { // clear entry in instr cache
 			icaches030[(regs.caar >> 4) & (CACHELINES030 - 1)].valid[(regs.caar >> 2) & 3] = 0;
 			regs.cacr &= ~0x04;
+#ifdef DEBUGGER
 			flush_cpu_cache_debug(regs.caar, CACHELINES030);
+#endif
 		}
 		if ((regs.cacr & 0x800) || force) { // clear data cache
 			if (doflush) {
@@ -1658,7 +1668,9 @@ void flush_cpu_caches(bool force)
 					icaches040[i].valid[j] = false;
 				}
 			}
+#ifdef DEBUGGER
 			flush_cpu_cache_debug(0, -1);
+#endif
 		}
 	}
 }
@@ -1768,7 +1780,9 @@ static void flush_cpu_caches_040_2(int cache, int scope, uaecptr addr, bool push
 						tagmask = cacheitag04060mask;
 						index = (addr >> 4) & cacheisets04060mask;
 						c = &icaches040[index];
+#ifdef DEBUGGER
 						flush_cpu_cache_debug(addr, 16);
+#endif
 					} else {
 						tagmask = cachedtag04060mask;
 						index = (addr >> 4) & cachedsets04060mask;
@@ -3125,7 +3139,9 @@ kludge_me_do:
 		return;
 	}
 	m68k_setpc(newpc);
+#ifdef DEBUGGER
 	branch_stack_push(currpc, currpc);
+#endif
 	regs.ir = x_get_word(m68k_getpc()); // prefetch 1
 	if (hardware_bus_error) {
 		if (nr == 2 || nr == 3) {
@@ -3722,7 +3738,9 @@ kludge_me_do:
 		set_special(SPCFLAG_END_COMPILE);
 	}
 #endif
+#ifdef DEBUGGER
 	branch_stack_push(currpc, nextpc);
+#endif
 	regs.ipl_pin = intlev();
 	ipl_fetch_now();
 	fill_prefetch ();
@@ -3737,9 +3755,11 @@ static void ExceptionX (int nr, uaecptr address, uaecptr oldpc)
 	regs.loop_mode = 0;
 
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 	if (debug_dma) {
 		record_dma_event_data(DMA_EVENT_CPUINS, current_hpos(), vpos, 0x20000);
 	}
+#endif
 #endif
 	if (cpu_tracer) {
 		cputrace.state = nr;
@@ -3857,9 +3877,10 @@ static int get_ipl(void)
 static void do_interrupt (int nr)
 {
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 	if (debug_dma)
 		record_dma_event(DMA_EVENT_CPUIRQ, current_hpos (), vpos);
-
+#endif
 	if (inputrecord_debug & 2) {
 		if (input_record > 0)
 			inprec_recorddebug_cpu (2);
@@ -4175,10 +4196,12 @@ uae_u32 REGPARAM2 op_illg (uae_u32 opcode)
 		}
 	}
 
+#ifdef DEBUGGER
 	if (debugmem_illg(opcode)) {
 		m68k_incpc_normal(2);
 		return 4;
 	}
+#endif
 
 	if (cloanto_rom && (opcode & 0xF100) == 0x7100) {
 		m68k_dreg (regs, (opcode >> 9) & 7) = (uae_s8)(opcode & 0xFF);
@@ -4646,9 +4669,13 @@ static void check_uae_int_request(void)
 			irq6 = true;
 		}
 		if (uae_int_requested & 0xff0000) {
+#ifdef WITH_PPC
 			if (!cpuboard_is_ppcboard_irq()) {
+#endif
 				atomic_and(&uae_int_requested, ~0x010000);
+#ifdef WITH_PPC
 			}
+#endif
 		}
 	}
 	if (irq2 || irq6) {
@@ -5049,10 +5076,12 @@ static void check_debugger(void)
 static void debug_cpu_stop(void)
 {
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 	record_dma_event(DMA_EVENT_CPUSTOP, current_hpos(), vpos);
 	if (time_for_interrupt()) {
 		record_dma_event(DMA_EVENT_CPUSTOPIPL, current_hpos(), vpos);
 	}
+#endif
 #endif
 }
 
@@ -5297,9 +5326,11 @@ static void m68k_run_1 (void)
 				}
 #endif
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 
 				r->instruction_pc = m68k_getpc ();
@@ -5463,16 +5494,20 @@ static void m68k_run_1_ce (void)
 #endif
 
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
 #endif
+#endif
 
 				r->instruction_pc = m68k_getpc ();
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_dma) {
 					record_dma_event_data(DMA_EVENT_CPUINS, current_hpos(), vpos, r->opcode);
 				}
+#endif
 #endif
 
 				(*cpufunctbl_noret[r->opcode])(r->opcode);
@@ -5500,7 +5535,9 @@ cont:
 					cputrace.needendcycles = 0;
 					write_log(_T("STARTCYCLES=%016llx ENDCYCLES=%016llx\n"), cputrace.startcycles, get_cycles());
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 					log_dma_record ();
+#endif
 #endif
 				}
 
@@ -6097,6 +6134,7 @@ static void opcodedebug (uae_u32 pc, uae_u16 opcode, bool full)
 		fault = 1;
 	} ENDTRY
 	if (!fault) {
+#ifdef DEBUGGER
 		TCHAR buf[100];
 		if (full)
 			write_log (_T("mmufixup=%d %04x %04x\n"), mmufixup[0].reg, regs.wb3_status, regs.mmu_ssw);
@@ -6104,6 +6142,7 @@ static void opcodedebug (uae_u32 pc, uae_u16 opcode, bool full)
 		write_log (_T("%s\n"), buf);
 		if (full)
 			m68k_dumpstate(NULL, 0xffffffff);
+#endif
 	}
 }
 
@@ -6142,8 +6181,10 @@ void cpu_halt(int id)
 			regs.intmask = 7;
 			MakeSR();
 			audio_deactivate();
+#ifdef DEBUGGER
 			if (debugging)
 				activate_debugger();
+#endif
 		}
 	}
 	set_special(SPCFLAG_CHECK);
@@ -6498,9 +6539,11 @@ static void m68k_run_3ce (void)
 					fill_icache040(r->instruction_pc + 16);
 
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 
 				(*cpufunctbl_noret[r->opcode])(r->opcode);
@@ -6597,9 +6640,11 @@ static void m68k_run_3p(void)
 					fill_icache040(r->instruction_pc + 16);
 
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 
 				(*cpufunctbl_noret[r->opcode])(r->opcode);
@@ -6802,9 +6847,11 @@ fprintf ( stderr , "cache valid %d tag1 %x lws1 %x ctag %x data %x mem=%x\n" , c
 						inprec_playdebug_cpu (1);
 				}
 
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 
 				(*cpufunctbl_noret[r->opcode])(r->opcode);
@@ -6975,9 +7022,11 @@ static void m68k_run_2p (void)
 						inprec_playdebug_cpu (1);
 				}
 
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 
 				if (currprefs.cpu_memory_cycle_exact) {
@@ -7130,9 +7179,11 @@ static void m68k_run_2_000(void)
 				count_instr (r->opcode);
 
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 
 				cpu_cycles = (*cpufunctbl[r->opcode])(r->opcode) & 0xffff;
@@ -7222,9 +7273,11 @@ static void m68k_run_2_020(void)
 				count_instr(r->opcode);
 
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_opcode_watch) {
 					debug_trainer_match();
 				}
+#endif
 #endif
 				cpu_cycles = (*cpufunctbl[r->opcode])(r->opcode) >> 16;
 				cpu_cycles = adjust_cycles(cpu_cycles);
@@ -7434,10 +7487,12 @@ void m68k_go (int may_quit)
 			/* We may have been restoring state, but we're done now.  */
 			if (isrestore ()) {
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 				if (debug_dma) {
 					record_dma_reset(0);
 					record_dma_reset(0);
 				}
+#endif
 #endif
 				restored = savestate_restore_finish ();
 #ifndef WINUAE_FOR_HATARI
@@ -7481,8 +7536,10 @@ void m68k_go (int may_quit)
 		if (changed_prefs.inprecfile[0] && input_record)
 			inprec_prepare_record (savestate_fname[0] ? savestate_fname : NULL);
 
+#ifdef DEBUGGER
 		if (changed_prefs.trainerfile[0])
 			debug_init_trainer(changed_prefs.trainerfile);
+#endif
 #endif
 
 		set_cpu_tracer (false);
@@ -7630,7 +7687,9 @@ void m68k_disasm_ea (uaecptr addr, uaecptr *nextpc, int cnt, uae_u32 *seaddr, ua
 	buf = xcalloc (TCHAR, (MAX_LINEWIDTH + 1) * pcnt);
 	if (!buf)
 		return;
+#ifdef DEBUGGER
 	m68k_disasm_2(buf, MAX_LINEWIDTH * pcnt, addr, NULL, 0, nextpc, cnt, seaddr, deaddr, lastpc, 1);
+#endif
 	xfree (buf);
 }
 void m68k_disasm (uaecptr addr, uaecptr *nextpc, uaecptr lastpc, int cnt)
@@ -7643,7 +7702,9 @@ void m68k_disasm (uaecptr addr, uaecptr *nextpc, uaecptr lastpc, int cnt)
 	buf = xcalloc (TCHAR, (MAX_LINEWIDTH + 1) * pcnt);
 	if (!buf)
 		return;
+#ifdef DEBUGGER
 	m68k_disasm_2(buf, MAX_LINEWIDTH * pcnt, addr, NULL, 0, nextpc, cnt, NULL, NULL, lastpc, 0);
+#endif
 	console_out_f (_T("%s"), buf);
 	xfree (buf);
 }
@@ -7683,6 +7744,7 @@ void m68k_dumpstate(uaecptr *nextpc, uaecptr prevpc)
 		regs.isp = m68k_areg (regs, 7);
 	j = 2;
 	console_out_f (_T("USP  %08X ISP  %08X "), regs.usp, regs.isp);
+#ifdef DEBUGGER
 	for (i = 0; m2cregs[i].regno>= 0; i++) {
 		if (!movec_illg (m2cregs[i].regno)) {
 			if (!_tcscmp (m2cregs[i].regname, _T("USP")) || !_tcscmp (m2cregs[i].regname, _T("ISP")))
@@ -7693,6 +7755,7 @@ void m68k_dumpstate(uaecptr *nextpc, uaecptr prevpc)
 			j++;
 		}
 	}
+#endif
 	if (j > 0)
 		console_out_f (_T("\n"));
 		console_out_f (_T("SR=%04X T=%d%d S=%d M=%d X=%d N=%d Z=%d V=%d C=%d IM=%d STP=%d\n"),
@@ -7736,8 +7799,10 @@ void m68k_dumpstate(uaecptr *nextpc, uaecptr prevpc)
 		}
 		for (int i = 0; i < 3; i++) {
 			uae_u16 w;
+#ifdef DEBUGGER
 			if (!debug_get_prefetch(i, &w))
 				break;
+#endif
 			struct instr *dp;
 			struct mnemolookup *lookup;
 			dp = table68k + w;
@@ -8829,6 +8894,7 @@ void do_cycles_stop(int c)
 		do_cycles(c);
 	} else {
 #ifndef WINUAE_FOR_HATARI
+#ifdef DEBUGGER
 		if (debug_dma) {
 			while (c > 0) {
 				debug_cpu_stop();
@@ -8836,8 +8902,11 @@ void do_cycles_stop(int c)
 				c -= CYCLE_UNIT;
 			}
 		} else {
+#endif
 			x_do_cycles(c);
+#ifdef DEBUGGER
 		}
+#endif
 #else
 		x_do_cycles(c);
 #endif
@@ -9918,6 +9987,7 @@ static void dcache030_maybe_burst(uaecptr addr, struct cache030 *c, int lws)
 	}
 }
 
+#ifdef DEBUGGER
 static uae_u32 read_dcache030_debug(uaecptr addr, uae_u32 size, uae_u32 fc, bool *cached)
 {
 	static const uae_u32 mask[3] = { 0x000000ff, 0x0000ffff, 0xffffffff };
@@ -9971,6 +10041,7 @@ static uae_u32 read_dcache030_debug(uaecptr addr, uae_u32 size, uae_u32 fc, bool
 	out &= mask[size];
 	return out;
 }
+#endif
 
 // [HATARI] Define next line to check for 68030 data cache mismatch after every write
 //#define WINUAE_FOR_HATARI_DEBUG_CACHE
@@ -10719,6 +10790,7 @@ static int dcache040_fill_line(int index, uae_u32 tag, uaecptr addr)
 	return line;
 }
 
+#ifdef DEBUGGER
 static uae_u32 read_dcache040_debug(uae_u32 addr, int size, bool *cached)
 {
 	int index;
@@ -10751,6 +10823,7 @@ nocache:
 		return get_word_debug(addr);
 	return get_long_debug(addr);
 }
+#endif
 
 static uae_u32 read_dcache040(uae_u32 addr, int size, uae_u32 (*fetch)(uaecptr))
 {
@@ -10953,6 +11026,7 @@ uae_u32 next_ilong_cache040(void)
 	return r;
 }
 
+#ifdef DEBUGGER
 uae_u32 get_byte_cache_debug(uaecptr addr, bool *cached)
 {
 	*cached = false;
@@ -10984,6 +11058,7 @@ uae_u32 get_long_cache_debug(uaecptr addr, bool *cached)
 	}
 	return get_long_debug(addr);
 }
+#endif
 
 void check_t0_trace(void)
 {
