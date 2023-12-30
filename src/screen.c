600f5bf73c73a34f26d5d104ef519897f0930bb4
Mihai Parparita <mihai@persistent.info>
2023-12-30 16:28:20 +0000

    Stop using SDL_GetWindowSizeInPixels
    Not implementing the version of SDL that I have installed, SDL_GetWindowSize
    works well enough for my needs.

--- src/screen.c
+++ src/screen.c
@@ -378,7 +378,7 @@ void Screen_Init(void) {
 		}
 	}
 
-	SDL_GetWindowSizeInPixels(sdlWindow, &nWindowWidth, &nWindowHeight);
+	SDL_GetWindowSize(sdlWindow, &nWindowWidth, &nWindowHeight);
 	if (nWindowWidth > 0) {
 		dpiFactor = (float)width / nWindowWidth;
 		fprintf(stderr,"SDL screen scale: %.3f\n", dpiFactor);
